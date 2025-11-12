9aa1d86c11e266150465974dbf9f01eac0dea62b
Niek van de Pas <n.vandepas@uu.nl>
2025-11-12 12:03:14 +0100

    add auto-save plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,16 @@
 -- See the kickstart.nvim README for more information
 return {
   {
+    "okuuva/auto-save.nvim",
+    version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
+  {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons'
