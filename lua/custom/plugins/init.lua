fdb90bbc7dc071672028cadb56a28293451ba436
Niek van de Pas <n.vandepas@uu.nl>
2025-11-18 11:25:44 +0100

    add github copilot plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -18,6 +18,9 @@ vim.api.nvim_create_autocmd('User', {
 
 return {
   {
+    "github/copilot.vim"
+  },
+  {
     "okuuva/auto-save.nvim",
     version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
     cmd = "ASToggle", -- optional for lazy loading on command
